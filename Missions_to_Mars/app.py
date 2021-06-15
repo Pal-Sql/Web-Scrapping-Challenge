f0ceb04bb0a81123d25d6dfc2ff745558e18f69c
Pallavi-Laptop <p.p.takalkhede@gmail.com>
2021-06-14 21:15:09 -0500

    html files

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-import time
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://redplanetscience.com")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars")
 
 
 # Route to render index.html template using data from Mongo
@@ -15,21 +14,22 @@ mongo = PyMongo(app, uri="mongodb://redplanetscience.com")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", data=mars_data)
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():
 
+def scrape():
+    
     # Run the scrape function
-    costa_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
+    mongo.db.mars_data.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
